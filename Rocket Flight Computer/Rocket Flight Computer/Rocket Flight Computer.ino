6a6314768bdf3631f48e94df2e3225491efb5101
Shubham Patayane <67165484+Shubham-Patayane@users.noreply.github.com>
2023-02-17 01:20:05 +0000

    Project Started
    Basic files created. The main program is yet to be written.

---
+++ Rocket Flight Computer/Rocket Flight Computer/Rocket Flight Computer.ino
@@ -0,0 +1,24 @@
+// Rocket Flight Computer Main code
+
+
+// ------- VERSION ---------
+// Latest Version - V0.1
+
+// V0.1 - Just the start of the code.
+
+
+
+#include <EEPROM.h>
+#include <Wire.h>
+#include <SPI.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
